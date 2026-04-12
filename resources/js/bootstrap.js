b13247d814d3d54ac6cba7563349fed33a88a28f
Isaac <IsaactimothyLyne@gmail.com>
2026-04-13 07:58:49 +1200

    vue & inertia setup

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import axios from "axios";
+// if window exists (for SSR) then set it to the global window object
+if (typeof window !== "undefined") {
+    window.axios = axios;
+    window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+}
